b4604d595a7874391c02d6a51cca02d61252c012
Ivan O <ioparin@pm.me>
2021-12-09 17:53:05 +0300

    initial package setup

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "libPhoneNumber",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "libphonenumber",
+            type: .static,
+            targets: ["libphonenumber"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libphonenumber",
+            path: "libphonenumber",
+            exclude: [],
+            sources: [
+                "Sources"
+            ],
+            publicHeadersPath: "Public",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreTelephony"),
+                .linkedLibrary("z")
+            ]
+        )
+    ]
+)
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# objc-libphonenumber-spm
+
+A fork of [iziz/libPhoneNumber-iOS](https://github.com/iziz/libPhoneNumber-iOS) updated to work flawlessly as the Swift Package library.
